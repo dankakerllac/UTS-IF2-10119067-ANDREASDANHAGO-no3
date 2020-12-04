8297129f130d120f6be646dafe60d06bc6c2f12b
User <User@DESKTOP-6N6EHH6>
2020-12-04 22:03:18 +0700

    PBOIF2-10119067

---
+++ src/uts/if2/pkg10119067/andreasdanhago/no3/Folk.java
@@ -0,0 +1,25 @@
+/*
+NAMA   : Andreas Danhago
+KELAS  : PBO2
+NIM    : Andreas Danhago
+Deskripsi Program : Genre Music
+ */
+package uts.if2.pkg10119067.andreasdanhago.no3;
+
+/**
+ *
+ * @author Zetro
+ */
+public class Folk extends Country{
+    public void genreFolk(String artistName) {
+        System.out.println(artistName+" adalah musisi Folk");
+    }
+
+    void setArtistName(String bob_Dylan) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getArtistName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
