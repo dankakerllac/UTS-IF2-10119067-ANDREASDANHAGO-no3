d5fdcd198c81c00542a8fe70db18df5ff8f084ce
User <User@DESKTOP-6N6EHH6>
2020-12-04 22:03:40 +0700

    PBOIF2-10119067

---
+++ src/uts/if2/pkg10119067/andreasdanhago/no3/Country.java
@@ -0,0 +1,17 @@
+/*
+NAMA   : Andreas Danhago P.I
+KELAS  : PBO2
+NIM    : 10119067
+Deskripsi Program : Genre Music
+ */
+package uts.if2.pkg10119067.andreasdanhago.no3;
+
+/**
+ *
+ * @author Zetro
+ */
+public class Country extends MusicGenre{
+    public void genreCountry(String artistName) {
+        System.out.println(artistName+" adalah musisi Country");
+    }
+}
