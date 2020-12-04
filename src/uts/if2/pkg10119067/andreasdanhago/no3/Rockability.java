34e2c42cd3cea40dc6911efdb29d09980aa753bc
User <User@DESKTOP-6N6EHH6>
2020-12-04 22:04:38 +0700

    PBOIF2-10119067

---
+++ src/uts/if2/pkg10119067/andreasdanhago/no3/Rockability.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if2.pkg10119067.andreasdanhago.no3;
+
+/**
+ *
+ * @author Zetro
+ */
+public class Rockability extends Country{
+    public void genreRockability(String artistName) {
+        System.out.println(artistName+" adalah musisi Rockability");
+    }
+}
